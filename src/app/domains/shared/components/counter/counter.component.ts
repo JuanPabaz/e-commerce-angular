4470cacc539891c3f189f75dc5cd9ab1a30b5550
JuanPabaz <jpgthf@gmail.com>
2024-11-07 09:10:02 -0500

    feat: add constructor and ngOnChanges()

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './counter.component.css'
 })
 export class CounterComponent {
+  @Input({required:true}) duration = 0;
+  @Input({required:true}) message = '';
 
+  constructor() {
+    // No debe ser asincrono
+    // Esto se corre antes de renderizar el componente
+    console.log('Constructor');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChanges){
+    // Se corre antes y durante renderizacion del componente
+    // O sea cada vez que hay un cambio en las propiedades
+    console.log('ngOnChanges');
+    console.log(changes);
+    console.log('-'.repeat(10));
+  }
 }
