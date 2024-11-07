5d978e6d32534003b0d484565da4ed92812a2794
JuanPabaz <jpgthf@gmail.com>
2024-11-07 10:17:07 -0500

    feat: add ngOnInit, ngAfterViewInit and ngOnDestroy

--- src/app/domains/shared/components/counter/counter.component.ts
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -14,6 +14,7 @@ export class CounterComponent {
   constructor() {
     // No debe ser asincrono
     // Esto se corre antes de renderizar el componente
+    // Una vez
     console.log('Constructor');
     console.log('-'.repeat(10));
   }
@@ -23,6 +24,29 @@ export class CounterComponent {
     // O sea cada vez que hay un cambio en las propiedades
     console.log('ngOnChanges');
     console.log(changes);
+    console.log('-'.repeat(10));
+  }
+
+  ngOnInit(){
+    // Se corre despues de renderizar el componente despues del constructor
+    // Una vez
+    // Puede ser asincrono
+    console.log('ngOnInit');
+    console.log('duration:', this.duration);
+    console.log('message:', this.message);
+    console.log('-'.repeat(10));
+  }
+
+  ngAfterViewInit(){
+    // Se corre despues de renderizar el componente
+    // Si los hijos de este componente ya fueron renderizados
+    console.log('ngAfterViewInit');
+    console.log('-'.repeat(10));
+  }
+
+  ngOnDestroy(){
+    // 
+    console.log('ngOnDestroy');
     console.log('-'.repeat(10));
   }
 }
