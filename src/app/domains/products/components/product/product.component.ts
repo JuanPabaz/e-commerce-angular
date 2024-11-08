d0de6d597784d33c05e585f62f2df592fa9ef112
JuanPabaz <jpgthf@gmail.com>
2024-11-08 11:42:22 -0500

    feat: send product to cart

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -21,7 +21,6 @@ export class ProductComponent {
   @Output() addToCart = new EventEmitter();
 
   addToCartHandler() {
-    console.log('click from child');
-    this.addToCart.emit('Hola este es un mensaje desde el hijo');
+    this.addToCart.emit(this.product);
   }
 }
