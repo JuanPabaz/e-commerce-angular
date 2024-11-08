386d79e3c1447afa3408ed9921b9c7e945de463c
JuanPabaz <jpgthf@gmail.com>
2024-11-08 11:43:59 -0500

    feat: add getTotalPrice and added to ngOnChanes

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,16 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, Input, signal, SimpleChanges } from '@angular/core';
+import { Product } from '../../models/product.model';
+import { CartproductComponent } from '../../../products/components/cartproduct/cartproduct.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CartproductComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
   hideSideCart = signal(true);
+  @Input({required: true}) cartProducts: Product[] = [];
+  totalPrice = signal(0);
+
+  ngOnChanges(changes: SimpleChanges){
+    this.getTotalPrice();
+
+  }
 
   toggleSideCart(){
     this.hideSideCart.update(prevState => !prevState);
+  }
+
+  getTotalPrice(){
+    this.totalPrice.update(() => this.cartProducts.reduce((acc, product) => acc + product.price, 0)); 
   }
 }
