fa4c7a25c883ab5c61b4cc9863783cc57c3b2254
JuanPabaz <jpgthf@gmail.com>
2024-11-08 09:14:05 -0500

    feat: update list component with a reactive way to show the products

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -9,6 +10,42 @@ import { ProductComponent } from '../../components/product/product.component';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Producto 1',
+        price: 100,
+        image: 'https://picsum.photos/640/640?r=23',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 2',
+        price: 200,
+        image: 'https://picsum.photos/640/640?r=24',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 3',
+        price: 300,
+        image: 'https://picsum.photos/640/640?r=25',
+        createdAt: new Date().toISOString()
+      },
+      {
+        id: Date.now(),
+        title: 'Producto 4',
+        price: 400,
+        image: 'https://picsum.photos/640/640?r=26',
+        createdAt: new Date().toISOString()
+      }
+    ];
+    this.products.set(initProducts);
+  }
+
   fromChild(event: string){
     console.log('Estamos en el padre')
     console.log(event)
