6ebbf1cd036bb407d2b4ed57dc63a5b0756ecf79
JuanPabaz <jpgthf@gmail.com>
2024-11-08 11:41:52 -0500

    feat: send cartProduct list to about component

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { CounterComponent } from '../../../shared/components/counter/counter.component';
 import { WaveAudioComponent } from '../../components/wave-audio/wave-audio.component';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-about',
@@ -12,6 +13,7 @@ import { HeaderComponent } from '../../../shared/components/header/header.compon
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  cartProducts = signal<Product[]>([]);
   duration = signal(1000);
   message = signal('Hola mundo');
   audioUrl = '../assets/audio.mp3';
