e1c348c42f7dcf730c95551f23203381485f68ff
JuanPabaz <jpgthf@gmail.com>
2024-11-08 08:52:17 -0500

    feat: update about page

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,17 +1,19 @@
 import { Component, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CounterComponent } from '../../../shared/components/counter/counter.component';
+import { WaveAudioComponent } from '../../components/wave-audio/wave-audio.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CommonModule,CounterComponent],
+  imports: [CommonModule,CounterComponent, WaveAudioComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
   duration = signal(1000);
   message = signal('Hola mundo');
+  audioUrl = '../assets/audio.mp3';
 
   changeDuration(event: Event){
     const input = event.target as HTMLInputElement;
